17f5acf2b37c929e853b41af1b7c0a6d2a3fd0b4
Tacocatman12 <jholsclaw@live.com>
2021-09-21 19:59:37 -0600

    added a the menu bar and added the beginnings of the introduction.

---
+++ portfoliosite/src/App.js
@@ -0,0 +1,14 @@
+import MainMenu from './MainMenu';
+import Introduction from './Introduction';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <MainMenu/>
+      <Introduction/>
+    </div>
+  );
+}
+
+export default App;
